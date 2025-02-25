dcbe94cafea32df0c0b1c994ea4619e1dc71bb8e
bichngoc94 <ngoc.tran@apptifact.vn>
2025-02-25 21:41:18 +0700

    fix Error in testing Loading.test.tsx

--- src/__test__/components/ui/Loading.test.tsx
+++ src/__test__/components/ui/Loading.test.tsx
@@ -1,9 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Loading from "../../../components/ui/Loading";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+// Create a mock store
+const mockStore = configureStore([]);
+const initialState = {
+  loading: {
+    isLoading: true,
+  },
+};
+const store = mockStore(initialState);
 
 describe("Loading Component", () => {
   test("renders the loading spinner", () => {
-    render(<Loading />);
+    render(
+      <Provider store={store}>
+        <Loading />
+      </Provider>
+    );
 
     // Check if the spinner is rendered
     const spinner = document.querySelector(".spinner"); // Assuming the spinner class is rendered
